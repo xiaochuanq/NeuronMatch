48b047aa400447d71237bb03f62aafbb6031fab9
Xiaochuan <xqin@seas.upenn.edu>
2011-02-10 22:55:02 -0500

    elipse project file and unit test file nclassify.cpp added

--- NeuronModel/neuron.cpp
+++ NeuronModel/neuron.cpp
@@ -1,10 +1,7 @@
 #include "neuron.h"
 
-NeuronNode::NeuronNode(int id, int type, float x, float y, float z, float d)
-{
-
-}
-
+/******************************** Neuron Cell/Tree *************************************/
 Neuron::Neuron()
 {
+
 }
--- NeuronModel/neuron.h
+++ NeuronModel/neuron.h
@@ -8,22 +8,24 @@
 class NeuronNode
 {
 	friend class Neuron;
-	friend std::istream& operator >>(std::istream &, NeuronNode &);
 public:
-    NeuronNode(int id, int type, float x, float y, float z, float d);
+    NeuronNode(int id, int type, float x, float y, float z, float d)
+		: m_nID(id), m_nType(type), m_v4coord(x, y, z, 0), m_fDiameter(d) {};
     int id(){ return m_nID; }
     int type(){ return m_nType; }
-    float x(){ return m_v4Coord.x;}
-    float y(){ return m_v4Coord.y;}
-    float z(){ return m_v4Coord.z;}
+    float diameter() { return m_fDiameter; }
     const Vector4 & coord(){ return m_v4Coord; }
-    void addChild(NeuronNode*);
+    void addChild(NeuronNode* pnn) { m_pNext.push_back(pnn); }
+private: //Do not implement purposely
+	NeuronNode();
+	NeuronNode(const NeuronNode&);
+	NeuronNode& operator=(const NeuronNode);
 private:
     std::list<NeuronNode*> m_pNext;
     int m_nID;
     int m_nType;
     Vector4 m_v4Coord;
-    float m_diameter;
+    float m_fDiameter;
 };
 
 class Neuron
@@ -31,15 +33,22 @@ class Neuron
 public:
     Neuron();
     void addNode();
-    void resample(std::vector<Vector4>&);
-    void vectorize(std::vector<Vector4>&);
+    void resample(float step);
+    void vectorize();
     const NeuronNode* root(){ return m_pSoma; };
+    int type();
+    const Vector4& zenith() { return m_v4zenith;}
+    const Vector4& azimuth() { return m_v4azimuth; }
 private:
+    int m_nType;
     NeuronNode* m_pSoma; // root;
+    Vector4 m_v4zenith;
+    Vector4 m_v4azimuth;
+    float m_fMaxR;
+    std::vector<Vector4> m_vv4Samples;
+    std::vector<Vector4> m_vv4Edges;
 };
 
-std::istream& operator>>(std::istream&, NeuronNode&);
 std::istream& operator>>(std::istream&, Neuron& );
-
 
 #endif // NEURON_H
